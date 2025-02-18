f2364a539833cadba8d5e6f05fd3004106a44d10
Pawel <pawel.zeja@infokam.kielce.pl>
2025-02-18 13:35:39 +0100

    subtitles, video_controller classes , got rid of global vars

--- SocketIOTest/__init__.py
+++ SocketIOTest/__init__.py
@@ -21,30 +21,94 @@ app = Flask(__name__)
 socketio = SocketIO(app)
 play = False
 
+
+
 # class Subtitles():
 #     def __init__(self) -> None:
+
+class Subtitles:
+    def __init__(self,text,text_times) -> None:
+        self.text = text
+        self.text_times = text_times
+        self.time_idx = 0
+
+    @staticmethod
+    def capture_subtitles_csv(filename ='E:/Programowanie/transcription.csv'):
+        text = []
+        text_times = []
+        with open(filename,'r',encoding='utf-8') as csv_file:
+            csvreader = csv.DictReader(csv_file)
+            for row in csvreader:
+                text.append({
+                    "text" : row["Sentence"]
+                    })
+                text_times.append((float(row["Start"]),float(row["Stop"])))
+        subtitles = Subtitles(text = text, text_times = text_times)
+        return subtitles
         
 
-def capture_subtitles():
-    """
-    Get subtitles text and times from csv file
-    """
+    def search_for_sub_idx(self,time):
+        """
+        Searching for subtitles index after rewinding film
+        """
+        i = 0
 
-    global text_times
-    subtitles = []
-    text_times = []
-    with open('E:/Programowanie/transcription.csv','r',encoding='utf-8') as csv_file:
-        csvreader = csv.DictReader(csv_file)
-        for row in csvreader:
-            subtitles.append({
-                "text" : row["Sentence"],
-                "start" : float(row["Start"]),
-                "stop" : float(row["Stop"])
-                })
-            text_times.append((float(row["Start"]),float(row["Stop"])))
-            
-    return subtitles, text_times
+        for i in range(len(self.text_times) - 1):
+            if time > self.text_times[i][0]:
+                if time < self.text_times[i + 1][0]:  
+                    self.time_idx = i
+                    
+            i+=1
+
+class VideoControler:
+    def __init__(self,cap,subtitles,video_length,fps) -> None:
+        self.cap = cap
+        self.subtitles = subtitles
+        self.play = False
+        self.video_length = video_length
+        self.fps = fps
+
+    def handle_rewind(self,time):
+   
+        self.subtitles.search_for_sub_idx(time)
+        self.cap.set(cv2.CAP_PROP_POS_MSEC, time * 1000)
+
+    def handle_start_stop(self,play):
+        self.play = play
+   
     
+cap = cv2.VideoCapture("E:/Programowanie/MOV2024.mp4")
+subtitles = Subtitles.capture_subtitles_csv()
+total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  
+fps = cap.get(cv2.CAP_PROP_FPS)  
+video_length = total_frames / fps
+
+app.video_controller = VideoControler(cap,subtitles,video_length,fps)        
+
+# def capture_subtitles():
+#     """
+#     Get subtitles text and times from csv file
+#     """
+
+#     global text_times
+#     subtitles = []
+#     text_times = []
+#     with open('E:/Programowanie/transcription.csv','r',encoding='utf-8') as csv_file:
+#         csvreader = csv.DictReader(csv_file)
+#         for row in csvreader:
+#             subtitles.append({
+#                 "text" : row["Sentence"],
+#                 "start" : float(row["Start"]),
+#                 "stop" : float(row["Stop"])
+#                 })
+#             text_times.append((float(row["Start"]),float(row["Stop"])))
+            
+#     return subtitles, text_times
+    
+    
+
+
+
 def put_text_on_image(img,text):
     """
     Putting text on frame
@@ -70,38 +134,42 @@ def put_text_on_image(img,text):
 
     return img
 
-def search_for_sub_idx(time):
-    """
-    Searching for subtitles index after rewinding film
-    """
-    i = 0
-    global cap, time_idx
-    for i in range(len(text_times) - 1):
-        if time > text_times[i][0]:
-            if time < text_times[i + 1][0]:  
-                time_idx = i
-                return time_idx
+# def search_for_sub_idx(time):
+#     """
+#     Searching for subtitles index after rewinding film
+#     """
+#     i = 0
+#     global cap, time_idx
+#     for i in range(len(text_times) - 1):
+#         if time > text_times[i][0]:
+#             if time < text_times[i + 1][0]:  
+#                 time_idx = i
+#                 return time_idx
 
-        i+=1
+#         i+=1
 
 def capture_frames():
     """Capture frames from the default camera and emit them to clients."""
     # cap = cv2.VideoCapture(0)
-    global cap,play,speed, video_length, text_times
+    
     # cap = get_frames_from_film(frame = 100)
-    video_path = "E:/Programowanie/MOV2024.mp4"
-    cap = cv2.VideoCapture(video_path)
+    # video_path = "E:/Programowanie/MOV2024.mp4"
+    # cap = cv2.VideoCapture(video_path)
+
+    cap = app.video_controller.cap
 
 
-    subtitles, text_time = capture_subtitles()
-    sub_idx = 0
-    time_idx = 0
+    # subtitles, text_time = capture_subtitles()
+    subtitles = app.video_controller.subtitles
+    
+    # sub_idx = 0
+    # time_idx = 0
 
 
 
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  
-    fps = cap.get(cv2.CAP_PROP_FPS)  
-    video_length = total_frames / fps  
+    # total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  
+    # fps = cap.get(cv2.CAP_PROP_FPS)  
+    # video_length = total_frames / fps  
     # eventlet.sleep(0.0001)
     # socketio.emit("set_max_time", {"max_time": int(video_length)})
 
@@ -111,7 +179,7 @@ def capture_frames():
 
     # text_time = subtitles[0]['start']
     while True:
-        if not play:
+        if not app.video_controller.play:
             eventlet.sleep(0.1)
             continue
 
@@ -122,14 +190,15 @@ def capture_frames():
 
         if not ret:
             # print("Error: Failed to capture frame.")
-            play = False
+            app.video_controller.play = False
             continue
-        if curr_time/1000 > text_time[time_idx][0]:
+       
+
+        if curr_time/1000 > subtitles.text_times[subtitles.time_idx][0]:
             
-            frame = put_text_on_image(frame,subtitles[sub_idx]['text'])
-            if curr_time/1000 > text_time[time_idx][1]:
-                sub_idx +=1
-                time_idx += 1
+            frame = put_text_on_image(frame,subtitles.text[subtitles.time_idx]['text'])
+            if curr_time/1000 > subtitles.text_times[subtitles.time_idx][1]:
+                subtitles.time_idx += 1
 
 
         # Encode the frame as JPEG
@@ -141,37 +210,39 @@ def capture_frames():
         curr_time_s = int(curr_time/1000)
         socketio.emit('frame', jpg_as_text)
         socketio.emit('curr_film_time', {"curr_time" : curr_time_s})
-        eventlet.sleep(1/(fps*1.5))
+        eventlet.sleep(1 / (app.video_controller.fps * 1.5))
         
 
     cap.release()
     
 import SocketIOTest.views
 
+
+
 @socketio.on("start")
 def handle_start():
     print(" Command: START")
-    global play
-    play = True
+    app.video_controller.handle_start_stop(play=True)
     socketio.emit("status", {"message": "Streaming started"})
-    socketio.emit("set_max_time", {"max_time": int(video_length)})
+    socketio.emit("set_max_time", {"max_time": int(app.video_controller.video_length)})
 
 @socketio.on("stop")
 def handle_stop():
     print(" Command: STOP")
-    global play
-    play = False
+    app.video_controller.handle_start_stop(play=False)
     socketio.emit("status", {"message": "Streaming stopped"})
 
 
 @socketio.on("rewind")
 def handle_rewind(data):
     print("Command: REWIND")
-    global cap, time_idx
+    global cap, time_idx, subtitles
     time = int(data['time'])
     
-    search_for_sub_idx(time)
-    cap.set(cv2.CAP_PROP_POS_MSEC, time * 1000)
+    app.video_controller.handle_rewind(time)
+    # app.video_controller.subtitles.search_for_sub_idx(time)
+    
+    # app.video_controller.cap.set(cv2.CAP_PROP_POS_MSEC, time * 1000)
    
     
     socketio.emit("status", {"message": f"Rewinding video to {time}s"})
